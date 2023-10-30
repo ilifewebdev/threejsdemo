458c56592b3ddb55759cdc13238b54aa8dab1e16
chao <shch1289@gmail.com>
2023-10-30 22:17:47 +0800

    feat:box

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [{ path: '/', name: 'Home', component: () => import('@/views/Home.vue') }]
+const routes = [
+  { path: '/', name: 'Home', component: () => import('@/views/home.vue') },
+  { path: '/box', name: 'box', component: () => import('@/views/box.vue') },
+]
 
 const router = createRouter({
   history: createWebHistory(),
